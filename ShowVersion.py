9e5a4c525d35113fdbadba5db8bcc39827ba5a80
hab-mart <tangkimhab@gmail.com>
2026-05-06 16:07:45 +0700

    Initial commit

---
+++ ShowVersion.py
@@ -0,0 +1,8 @@
+import sys
+import numpy as np
+import pandas as pd
+import sklearn
+print("Python:", sys.version)
+print("NumPy:", np.__version__)
+print("pandas:", pd.__version__)
+print("scikit-learn:", sklearn.__version__)
